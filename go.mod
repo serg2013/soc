09ec8a07a8adf61e3240329098da04e538a7265b
sergio2013 <raven1901@mail.ru>
2024-08-22 10:52:14 +0300

    GET|POST added

--- go.mod
+++ go.mod
@@ -5,8 +5,13 @@ go 1.22.2
 require github.com/go-chi/chi/v5 v5.1.0
 
 require (
+	github.com/PuerkitoBio/goquery v1.9.2
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/google/uuid v1.6.0
 )
 
-require golang.org/x/sys v0.4.0 // indirect
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
 	//"strconv"
 	"time"
 
@@ -49,9 +52,75 @@ func main() {
                 }
                 //log.Println("event:", event)
                 if event.Has(fsnotify.Create) {
-					fmt.Printf("New notification: %s\n", event.Name)
+					//fmt.Printf("New notification: %s\n", event.Name)
+						if strings.Contains(event.Name, "worker") {
+							var hRworker hubRequest
+							dSearch, err := os.Open("./"+event.Name)
+							if err != nil {
+								log.Fatal(err)
+							}
+							//fmt.Print(string(dat))
+							//err = json.Unmarshal(dat, &hRworker)
+							err = json.NewDecoder(dSearch).Decode(&hRworker)
+							if err != nil {
+								log.Fatal(err)
+							}
+							//fmt.Print(hRworker)
+							dSearch.Close()
+
+							go searchGoogle(hRworker)
+
+							eRem := os.Remove("./"+event.Name) 
+							if eRem != nil { 
+								log.Fatal(eRem) 
+							}
+							hRworker = hubRequest{}
+						}
+
+						if strings.Contains(event.Name, "done") {
+							dat, err := os.ReadFile("./"+event.Name)
+							if err != nil {
+								log.Fatal(err)
+							}
+							/*
+							/////////////////////////
+							var otRS outPutResult
+							fSendRes, err := os.Open("./"+event.Name)
+							if err != nil {
+								log.Fatal(err)
+							}
+							//fmt.Print(string(dat))
+							//err = json.Unmarshal(dat, &hRworker)
+							err = json.NewDecoder(fSendRes).Decode(&otRS)
+							if err != nil {
+								log.Fatal(err)
+							}
+							//fmt.Print(string(dat))
+							fSendRes.Close()
+							fmt.Print(string(dRes))
+							*/
+							
+							go sendResultsToHub(dat)
+
+						}
 					//////////////////////
 					// start workers in new pool routine?
+					/*
+					f, err := os.Create("tasks/worker/"+item.TaskId + "." + item.UUID.String() + "." + time.Now().Format("2006-01-02T15:04:05") + ".json")
+	
+					if err != nil {
+						log.Fatal(err)
+					}
+					
+					b, err := json.Marshal(item)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					//test
+					f.Write(b)
+					f.Close()
+					*/
 					//////////////////////
                 }
             case err, ok := <-watcher.Errors:
@@ -80,12 +149,17 @@ type hubRequest struct { //struct for post data
 	ItemId     	string		`json:"itemId"`
 }
 
+type taskIdResponse struct {
+	TaskId	string	`json:"task"`
+}
+
 type CustomTime struct {
 	time.Time
 }
 
 func (t *CustomTime) UnmarshalJSON(b []byte) (err error) {
-    date, err := time.Parse(`"2006-01-02T15:04:05"`, string(b))
+	//s := strings.Trim(string(b), `"`)
+    date, err := time.Parse(`"2006-01-02"`, string(b))
 	//date, err := time.Parse(`"2006-01-02"`, string(b))
     if err != nil {
         return err
@@ -94,30 +168,41 @@ func (t *CustomTime) UnmarshalJSON(b []byte) (err error) {
     return
 }
 
+func (t *CustomTime) MarshalJSON() ([]byte, error) {
+	if t.Time.IsZero() {
+		return nil, nil
+	}
+	return []byte(fmt.Sprintf(`"%s"`, t.Time.Format("2006-01-02"))), nil
+}
+
 func (t *CustomTime) GoogleParameterDate() string {
     return t.Format("2006-01-02")
 }
 
 type outPutLinks struct {
-	Link     	string	`json:"link"`
-	IndexedDate string	`json:"indexedDate"`
+	Link				string	`json:"link"`
+	IndexedDate			string	`json:"indexedDate"`
+	IndexedDateFormat	string 	`json:"indexedDateFormat"`
 }
 
 type outPutSources struct { //struct for post data
-	Source			string   	`json:"sourceName"` // example: ff81ac90-51b6-42e5-b42e-59b72e4b45d2
-	Links		[]outPutLinks	`json:"links"`
-	Total			int   		`json:"total"`
+	Source	string   		`json:"sourceName"` // example: ff81ac90-51b6-42e5-b42e-59b72e4b45d2
+	Links	[]outPutLinks	`json:"links"`
+	Total	int   			`json:"total"`
 }
 
 type outPutResult struct { //struct for post data
-	TaskId			string   	`json:"task"` // example: ff81ac90-51b6-42e5-b42e-59b72e4b45d2
-	Sources		[]outPutSources	`json:"sources"`
+	TaskId			string   		`json:"task"` // example: ff81ac90-51b6-42e5-b42e-59b72e4b45d2
+	Sources			[]outPutSources	`json:"sources"`
+	TotalResults	int				`json:"totalResults"`
 }
 
 func startWS() { //start webserver and wait for post data in "/" route
 	router := chi.NewRouter()
+	
 	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
 		var item hubRequest
+		var resp taskIdResponse
 		err := json.NewDecoder(r.Body).Decode(&item)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
@@ -126,21 +211,73 @@ func startWS() { //start webserver and wait for post data in "/" route
 		}
 
 		item.UUID = uuid.New()
-		//item.PeriodStart = item.PeriodStart.GoogleParameterDate()
-		go createTask(item) // make go routine from this func?
-
-		fmt.Println("searchGoogle start")
-		var outRes outPutResult = searchGoogle(item)
-		fmt.Println(outRes)
-
-		jsonItem, err := json.Marshal(outRes)
+		
+		resp.TaskId = item.TaskId
+		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("illegal json"))
 			return
 		}
-		w.Write(jsonItem)
-		fmt.Printf("Task %s in progress\n", item.TaskId)
+		w.Write(jsonResp)
+
+		go createTask(item) // make go routine from this func?
+
+		//var outRes outPutResult = searchGoogle(item)
+
+		//go searchGoogle(item)/////////////////////////////////////////////////////////////////////////
+
+		// jsonItem, err := json.Marshal(outRes)
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusInternalServerError)
+		// 	w.Write([]byte("illegal json"))
+		// 	return
+		// }
+		// w.Write(jsonItem)
+
+	})
+
+	router.Post("/test/", func(w http.ResponseWriter, r *http.Request) {
+		var chP outPutResult
+		err := json.NewDecoder(r.Body).Decode(&chP)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		jsonResp, err := json.Marshal(chP)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("illegal json"))
+			return
+		}
+		w.Write(jsonResp)
+		//fmt.Println(chP)
+		chP = outPutResult{}
+	})
+
+	router.Get("/{task}", func(w http.ResponseWriter, r *http.Request) {
+		
+		//var resOutPut outPutResult
+
+		task_id := chi.URLParam(r, "task") // 👈 getting path param
+
+		dat, err := os.ReadFile("./tasks/done/"+task_id+".json")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		/*
+		jsonResp, err := json.Marshal(resOutPut)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("illegal json"))
+			return
+		}
+		w.Write(jsonResp)
+		*/
+		w.Write(dat)
+		//opRes.Close()
 	})
 
 	err := http.ListenAndServe(":3000", router)
@@ -164,7 +301,7 @@ func createFolders() {
 		if errDir != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("Folders created")
+		//fmt.Println("Folders created")
 	}
 }
 
@@ -174,14 +311,31 @@ func createTask(item hubRequest) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	b, err := json.Marshal(item)
+	//fmt.Println(item)
+	err = json.NewEncoder(f).Encode(&item)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	f.Write(b)
+	//f.Write(b)
+	f.Close()
+}
+
+func writeTaskResults (it outPutResult) {
+	f, err := os.Create("tasks/done/"+it.TaskId + ".json")
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(it)
+	err = json.NewEncoder(f).Encode(&it)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//f.Write(b)
 	f.Close()
 }
 
@@ -207,52 +361,172 @@ type outPutResult struct { //struct for post data
 */
 
 
-func searchGoogle (it hubRequest) outPutResult {
+
+/*
+func rusToEngMonthFormat (sD string) string {
+	
+	/*monthsMap := map[string]string {
+		"янв.":	"January",
+		"фев.":	"February",
+		"мар.":	"March",
+		"апр.":	"April",
+		"мая":	"May",
+		"июн.":	"June",
+		"июл.":	"July",
+		"авг.":	"August",
+		"сен.": "September",
+		"окт.":	"October",
+		"ноя.":	"November",
+		"дек.":	"December",
+	}
+	fmt.Println("sD:  ")
+	fmt.Println(sD)
+	monthsNumMap := map[string]string {
+		"янв.":	"01",
+		"фев.":	"02",
+		"мар.":	"03",
+		"апр.":	"04",
+		"мая":	"05",
+		"июн.":	"06",
+		"июл.":	"07",
+		"авг.":	"08",
+		"сен.": "09",
+		"окт.":	"10",
+		"ноя.":	"11",
+		"дек.":	"12",
+	}
+
+	var res string
+
+	rsD := strings.Split(sD, " ")
+	if len(rsD[0]) < 2 {
+		//res = "0" + rsD[0] + " " + monthsNumMap[rsD[1]] + " " + rsD[2]
+		res = rsD[2] + "-" + monthsNumMap[rsD[1]] + "-" + "0" + rsD[0]
+	}
+
+	fmt.Println(res)
+
+	return res
+}
+*/
+
+func sendResultsToHub (b []byte) {
+	// var otRS outPutResult
+	// fmt.Print(string(b))
+	// err := json.Unmarshal(b, otRS)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	
+	client := &http.Client{}
+	url := "http://localhost.ru/test/:3000"
+	////send post
+	//jsonSend, err := json.Marshal(b)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(b)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header = http.Header {
+		"Accept": {"application/json"},
+		"Accept-Language": {"ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3"},
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res.Body.Close()
+
+}
+
+
+//func searchGoogle (it hubRequest) outPutResult {
+func searchGoogle (it hubRequest) {
+	time.Sleep(1 * time.Second)
 	var outResult outPutResult
 	outResult.TaskId = it.TaskId
+	outResult.TotalResults = 0
 
-	var outSrcs outPutSources
+	monthsNumMap := map[string]string {
+		"янв.":	"01",
+		"фев.":	"02",
+		"мар.":	"03",
+		"апр.":	"04",
+		"мая":	"05",
+		"июн.":	"06",
+		"июл.":	"07",
+		"авг.":	"08",
+		"сен.": "09",
+		"окт.":	"10",
+		"ноя.":	"11",
+		"дек.":	"12",
+	}
 
-	var outLks outPutLinks
+	var rsD []string
 
 	client := &http.Client{}
-	for _, value := range it.SourceList {
-		outSrcs.Source = value
+	for iv, value := range it.SourceList {
+		
+		var outSrcs outPutSources
+		var outLks outPutLinks
+
+		outSrcs.Source = it.SourceList[iv]
+		//println(outSrcs.Source)
 		//Do something with index and value
 		url := "https://www.google.com/search?q="+it.ItemId+"+site:"+value+"+after:"+it.PeriodStart.GoogleParameterDate()+"+before:"+it.PeriodEnd.GoogleParameterDate()+"&num=100"
-		fmt.Println(url)
+		//fmt.Println(url)
 		req, err := http.NewRequest("GET", url, nil)
 	    if err != nil {
 	    	log.Fatal(err)
 	    }
-		//totalIndex = index+1
-		req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36")
 		
+		req.Header = http.Header {
+			"User-Agent": {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"},
+			"Accept": {"application/json"},
+			"Accept-Language": {"ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3"},
+		}
+
  		res, err := client.Do(req)
 		if err != nil {
 			log.Fatal(err)
 		}
- 		defer res.Body.Close()
+ 		//defer res.Body.Close()
 
  		doc, err := goquery.NewDocumentFromReader(res.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		c := 0
+		//fmt.Println(doc)
+		outSrcs.Total = 0
+		//c := 0
 		doc.Find("div.g").Each(func(i int, result *goquery.Selection) {
 			outLks.Link, _ = result.Find("a").First().Attr("href")
-			outLks.IndexedDate, _ = result.Find(".Sqrs4e").Attr("class")
-			//outLks.IndexedDate = "1"
-			//fmt.Println(outLks.indexedDate)
-			fmt.Println(outLks)
+			outLks.IndexedDate = result.Find("span.Sqrs4e").Contents().First().Text()
+			
+			rsD = strings.Split(outLks.IndexedDate, " ")
+			rsYear := rsD[2]
+			
+			if len(rsD[0]) < 2 {
+				outLks.IndexedDateFormat = rsYear[0:4] + "-" + monthsNumMap[rsD[1]] + "-" + "0" + rsD[0]
+			}
+			outLks.IndexedDateFormat = rsYear[0:4] + "-" + monthsNumMap[rsD[1]] + "-" + rsD[0]
+			
 			outSrcs.Links = append(outSrcs.Links, outLks)
-			c++
-			outSrcs.Total = c
+			//c++
+			//outSrcs.Total = c
+			outSrcs.Total++
+			outResult.TotalResults++
+			//fmt.Println(outResult.TotalResults)
 		})
-		fmt.Println(outSrcs)
+		//fmt.Println(outSrcs)
 		outResult.Sources = append(outResult.Sources, outSrcs)
+		
+		res.Body.Close()
 	}
-	return outResult
+	//fmt.Println(outResult)
+	writeTaskResults(outResult)
 }
 
 /*
